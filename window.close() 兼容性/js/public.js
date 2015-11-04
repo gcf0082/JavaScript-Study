7bccee59d6421b7c07f775a8beca561a006d9fd1
sunshine9 <1104506375@qq.com>
2015-11-04 17:28:23 +0800

    window.close() 浏览器兼容性

---
+++ window.close() 兼容性/js/public.js
@@ -0,0 +1,41 @@
+﻿window.onload = function () {
+    // 5秒后跳转到起始页面 
+    var close_time = 5,
+        history_url = "",
+        url = " ";
+    timedCount();
+    function timedCount() {
+        document.getElementById('close_time').innerHTML = close_time;
+        close_time = close_time - 1;
+        var t = setTimeout(function () { timedCount() }, 1000);
+        if (close_time < 0) {
+            if (history_url == "" || null || undefined) {
+                if (navigator.userAgent.indexOf("MSIE") > 0) {
+                    if (navigator.userAgent.indexOf("MSIE 6.0") > 0) {
+                        window.opener = null; window.close();
+                    }
+                    else {
+                        window.open('', '_top');
+                        window.top.close();
+                    }
+                }
+                else if (navigator.userAgent.indexOf("Firefox") > 0) {
+                    window.location.href = 'about:blank';
+                }
+                else {
+                    window.opener = null;
+                    window.open(url, '_self');
+                    window.close();
+                    // parent.window.opener = null;
+                    // parent.window.open("", "_self");
+                    // parent.window.close();
+                }
+            }
+            else {
+                location.href = history_url;
+            }
+        }
+    }
+}
+
+
---
+++ 廖雪峰JS教城笔记/2015-11-2.js
@@ -0,0 +1,48 @@
+﻿// 包装对象
+var n = new Number(123); // 123,生成了新的包装对象
+var b = new Boolean(true); // true,生成了新的包装对象
+var s = new String("str"); // "str",生成了新的包装类型
+
+typeof new Number(123); // "object"
+new Number(123) === 123; // false
+
+typeof new Boolean(true); // "object"
+new Boolean(true) === true; // false
+
+typeof new String("str"); // "object"
+new String("str") === "str"; // false
+
+// Date()
+var now = new Date();
+now; // Wed Jun 24 2015 19:49:22 GMT+0800 (CST)
+now.getFullYear(); // 年份
+now.getMonth(); // 月份
+now.getDate(); // 日期
+now.getDay(); // 星期数
+now.getHours(); // 星期数
+now.getMinutes(); // 分钟
+now.getSeconds(); // 秒
+now.getMilliseconds(); // 毫秒数
+now.getTime();  // 以number形式表示的时间戳
+
+// 注意：当前时间是浏览器从本机操作系统获取的时间，所以不一定准确
+// 方法1：
+var d = new Date(2015, 5, 19, 20, 15, 30, 123);
+d; // Fri Jun 19 2015 20:15:30 GMT+0800 (CST);
+// 注意:JavaScript的月份范围用整数表示是0-11
+
+// 方法2：
+var d = Data.parse("2015-06-24T19:49:22.875+08:00");
+
+
+// 时区
+var d = new Date(1435146562875);
+d.toLocaleString();  // 当地时间
+d.toUTCString(); // 标准时间
+
+// 获取当前时间戳
+if (Date.now) {
+    alert(Date.now());  // 包版本IE没有now()方法
+} else {
+    alert(new Date().getTime());
+}
