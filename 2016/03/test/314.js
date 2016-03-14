f994cf53ee399d127af0eaeb14851d22a39d214f
Sun Lifeng <1104506375@qq.com>
2016-03-14 18:44:54 +0800

    test

--- 2016/03/test/314.js
+++ 2016/03/test/314.js
@@ -27,3 +27,44 @@ var func = object.getNameFunc();
 //alert('2.'+func()); // The Window
 // alert('3.'+func.call(object)); // My object
 alert('4.' + func.apply(object)); // My object
+
+// 数组合并，要求去重
+Array.prototype.contact1 = function() {
+    var res = [this[0]];
+    for (var i = 1; i < this.length; i++) {
+        var repeat = fasle;
+        for (var j = 0; j < res.length; j++) {
+            if (this[i] == res[j]) {
+                repeat = true;
+                break;
+            }
+        }
+        if (!repeat) {
+            res.push(this[i]);
+        }
+    }
+    return res;
+}
+
+Array.prototype.contact2 = function() {
+    this.sort(); // 先排序
+    var res = [this[0]];
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] == res[res.length - 1]) {
+            res.push(this[i]);
+        }
+    }
+    return res;
+}
+
+Array.prototype.contact3 = function() {
+    var res = [];
+    var json = {};
+    for (var i = 0; i < this.length; i++) {
+        if (!json[this[i]]) {
+            res.push(this[i]);
+            json[this[i]] = 1;
+        }
+    }
+    return res;
+}
